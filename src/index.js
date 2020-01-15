82c912321c4061f58264898545dea97a530a222a
Kate Hooppell <katehooppell@gmail.com>
2020-01-15 10:11:48 -0500

    adding react-axe to look into accessibility tools

--- src/index.js
+++ src/index.js
@@ -3,12 +3,32 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
- );
+
+
+
+if (process.env.NODE_ENV !== 'production') {
+  console.log(process.env.NODE_ENV);
+  import('react-axe').then(axe => {
+    axe.default(React, ReactDOM, 1000);
+    ReactDOM.render(<BrowserRouter>
+                      <App />
+                      </BrowserRouter>,
+                    document.getElementById('root'));
+  });
+} else {
+  ReactDOM.render(<BrowserRouter>
+                    <App />
+                    </BrowserRouter>,
+                  document.getElementById('root'));
+}
+
+
+// ReactDOM.render(
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>,
+//   document.getElementById('root')
+//  );
 
 
  if (module.hot) {
